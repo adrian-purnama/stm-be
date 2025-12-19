d2895e652540c406e38539164b0f6ece408c8f87
unknown <adrianpurnama209@gmail.com>
2025-12-19 10:42:25 +0700

    allow all cors

--- server.js
+++ server.js
@@ -23,12 +23,14 @@ const PORT = process.env.PORT || 5000;
 //   credentials: true
 // }));
 
-app.use(cors({
-  origin: true,
-  credentials: true,
+const corsOptions = {
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
-}));
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 
 app.use(express.json());
